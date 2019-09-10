80636085128543950b0ba955cffaccb98db077a1
bryanblancas <orionmunecaycanica@gmail.com>
2019-09-09 21:20:59 -0500

    Changed ip of blocking url

--- background.js
+++ background.js
@@ -52,7 +52,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 			}
 		}
 		},
-	{urls: ["*://10.100.64.48/login/byChaffing"]},
+	{urls: ["*://25.7.126.53/login/byChaffing"]},
 	["blocking", "requestHeaders"]
 );
 
