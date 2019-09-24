69f385d3d54ed7d887ab8bc3a99e81cbbc800cb2
bryanblancas <orionmunecaycanica@gmail.com>
2019-09-24 13:26:07 -0500

    Added implementation of base64 encode from wiki miselanious

--- background.js
+++ background.js
@@ -178,9 +178,19 @@ function makeChaffingBite(patternChaffing, certArray, cabeceraInyectar, details)
 	let stringBytesChaffingCertificado = arrayBytesToBites(stringChaffingCertificado, false);
 	console.log("CHAFFING CON CARACTERES ESPECIALES: "+stringBytesChaffingCertificado);
 	
+
 	//PASAR A BASE64 EL CHAFFING
-	//stringBytesChaffingCertificado = btoa(stringBytesChaffingCertificado);
-	stringBytesChaffingCertificado = encodeURI(stringBytesChaffingCertificado)
+	one = btoa(stringBytesChaffingCertificado);
+	two = base64_encode(stringBytesChaffingCertificado);
+	
+	if(one === two)
+		console.log("son iguales");
+	else
+		console.log("no son iguales");
+	
+
+
+	stringBytesChaffingCertificado = btoa(stringBytesChaffingCertificado)
 	console.log("CHAFFING EN BYTES (BASE64): " + stringBytesChaffingCertificado + " " + stringBytesChaffingCertificado.length);
 
 	//Se cambia el valor de Accept con el Chaffing del certificado
@@ -374,3 +384,41 @@ function analyzeSubstring(string){
 	return stringArray.join("");
 }
 
+
+
+function base64_encode (s)
+{
+  // the result/encoded string, the padding string, and the pad count
+  var base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+  var r = ""; 
+  var p = ""; 
+  var c = s.length % 3;
+
+  // add a right zero pad to make this string a multiple of 3 characters
+  if (c > 0) { 
+    for (; c < 3; c++) { 
+      p += '='; 
+      s += "\0"; 
+    } 
+  }
+
+  // increment over the length of the string, three characters at a time
+  for (c = 0; c < s.length; c += 3) {
+
+    // we add newlines after every 76 output characters, according to the MIME specs
+    if (c > 0 && (c / 3 * 4) % 76 == 0) { 
+      r += "\r\n"; 
+    }
+
+    // these three 8-bit (ASCII) characters become one 24-bit number
+    var n = (s.charCodeAt(c) << 16) + (s.charCodeAt(c+1) << 8) + s.charCodeAt(c+2);
+
+    // this 24-bit number gets separated into four 6-bit numbers
+    n = [(n >>> 18) & 63, (n >>> 12) & 63, (n >>> 6) & 63, n & 63];
+
+    // those four 6-bit numbers are used as indices into the base64 character list
+    r += base64chars[n[0]] + base64chars[n[1]] + base64chars[n[2]] + base64chars[n[3]];
+  }
+   // add the actual padding string, after removing the zero pad
+  return r.substring(0, r.length - p.length) + p;
+}
