0e7c64af4f1596601a3ae462e2713f443e6b066f
bryanblancas <orionmunecaycanica@gmail.com>
2019-10-08 16:23:05 -0500

    IP's

--- js/paginas/IPClase.js
+++ js/paginas/IPClase.js
@@ -1,7 +1,7 @@
 class IPClase {
     constructor(){
         //var dirIP = '25.7.11.142';
-        var dirIP = '192.168.0.11';
+        var dirIP = '10.100.68.109';
         //var dirIP = '192.168.43.179';
         //var dirIP = '10.140.99.39';
         this.dir = 'https://'+dirIP+':3000/api/';
