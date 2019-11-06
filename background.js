bd0173b9c1e9af8d96002a4d9339ba5340946444
bryanblancas <orionmunecaycanica@gmail.com>
2019-11-06 14:27:03 -0600

    Minor fixes:   Deleted comments without sense   Renamed variables   Deleted code without sense   Redefinition of some functions   Deleted old useless functions

--- background.js
+++ background.js
@@ -5,7 +5,9 @@ var btnActivar = "";
 // Funcion de la API de Google Chrome
 // Se ejecuta en cuando los encabezados de la petición HTTP se han creado
 chrome.webRequest.onBeforeSendHeaders.addListener(
-	function(details){
+	
+	function(headers){
+
 		// Función del API de Google Chrome que obtiene el valor guardado en el Storage
 		chrome.storage.local.get(['Activo'],function(result){
 			if(result.Activo == null){
@@ -18,44 +20,26 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 		// En caso de que el botón esté activo, se obtienen los datos del encabezado HTTP
 		// de la petición creada por el usuario
 		if (btnActivar == true){
-			let detailsComplete = details;
-			let requestid = details.requestId;
-			let method = details.method;
-			let frameid = details.frameId;
-			let parentframeid = details.parentFrameId;
-			let tabid = details.tabId;
-			let initiator = details.initiator;
-			let timestamp = details.timeStamp;
-			let url = details.url;
-			let httpheaders = details.requestHeaders;
-			let tipo = details.type;
+			let httpheaders = headers.requestHeaders;
+			let tipo = headers.type;
 
 			// Filtro para sólo obtener la petición tipo "main_frame"
 			if(tipo.includes("main_frame")){
 				
-				let cabeceraInyectar = "";
-
-				for(let i = 0; i < httpheaders.length; i++){
-					if(httpheaders[i].name.includes("Accept")){
-						cabeceraInyectar = httpheaders[i].value;
-					}
-				}
-				
-				//console.log("ENCABEZADO A MODIFICAR: \n"+cabeceraInyectar);
-				getCertificateFromStorage(cabeceraInyectar, detailsComplete);
+				getCertificateFromStorage(headers);
 				
 				// Bloquear petición
 				return {cancel: true};
 			}
 		}
-		},
+	},
 	{urls: ["*://localhost/login"]},
 	["blocking", "requestHeaders"]
 );
 
 
 // Función que obtiene el certificado del Storage e inicia el proceso de software
-function getCertificateFromStorage(cabeceraInyectar, details) {
+function getCertificateFromStorage(headers) {
 	
 	let certificadoCharArray = [];
 
@@ -65,20 +49,20 @@ function getCertificateFromStorage(cabeceraInyectar, details) {
 		if(result.cert != null){
 			
 			certificadoCharArray = result.cert;
-			//console.log("CERTIFICADO: \n"+certificadoCharArray);
+				
+			chaffingProcess(certificadoCharArray, headers);
 
-			chaffingProcess(certificadoCharArray, cabeceraInyectar, details);
 		}
+		else
+			alert("ERROR AL OBTENER CERTIFICADO DE STORAGE");
 
 	});	
 }
 
 
 // Función que realiza el proceso de chaffing
-function chaffingProcess(certificadoCharArray, cabeceraInyectar, details) {
-	// cert = certificadoCharArray.replace('\r\n', '');
-	// cert = cert.replace('\r', '');
-
+function chaffingProcess(certificadoCharArray, headers) {
+	
 	//REMOVE ESPECIAL CHARACTERS 
 	cert = []
 	for(let i = 0; i < certificadoCharArray.length; i++){
@@ -87,25 +71,17 @@ function chaffingProcess(certificadoCharArray, cabeceraInyectar, details) {
 			cert.push(certificadoCharArray[i])
 	}
 
-	// certificadoCharArray = "5\n";
-	//cabeceraInyectar = "t";
-	
 	let rtn = getPattern(950, 1050);
 	let pattern = rtn[0];
 	let ones = rtn[1];
-
 	// console.log(pattern)
 	// console.log(ones)
 
-	//let newHeader = makeChaffingBite(patternChaffing,certificadoCharArray,cabeceraInyectar,details);
-	//console.log("NUEVA PETICIÓN HTTP: ");
-	//console.log(newHeader);
-	
 	let chaffing = makeChaffing(pattern, cert, ones);
 	// console.log(chaffing);
 
 	//Liberación de la petición
-	freeRequest(details, chaffing, pattern, cert.length);
+	freeRequest(headers, chaffing, pattern, cert.length);
 }
 
 
@@ -194,14 +170,12 @@ function fakeChar(){
 
 
 // Función que libera la nueva petición con el chaffing y pattern dados
-function freeRequest(details, chaffing, pattern, len_cert){
-    
+function freeRequest(headers, chaffing, pattern, len_cert){
 
-    const url = details.url;
+    const url = headers.url;
     chaffing = chaffing.join('');
     pattern = pattern.join('');
-
-
+	console.log("PATTERN BITS: \n"+pattern)
 
     // pattern = text 
     // pattern = "01110100011001010111100001110100"
@@ -213,7 +187,6 @@ function freeRequest(details, chaffing, pattern, len_cert){
     var encrypt = new JSEncrypt();
 	encrypt.setPublicKey("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1fsvhHzUiUB20kciWsdCPf9gBiI6Z2cXnOH+VMkQtwXYhWyf0VXnV/cXGieXS5HvrZJvb0ldo8ZSqkaBy9BXrIAFswTgOxWfusa3nmL6YRzIHxI6FgpAt9xQAIKtnEWMShsufS/7FeR8Yam/u2qI2u+kM00ZPKQPOZPGQvEjy2QX88k/r88jP2a5UPzkSfg1vuAwMxGrVSuPcGrAUd2qJF6Slb1y6KvSo2KYLdnpv/us5MRKO+28u2QNr++uMIkyJz4Pqj67VUT2r1XThkdxAfPTgcRne15qQ2aDtlLqw8T6uo2xYNekZjuoUxenfxzikv3ejFgShT4bON6yzBtv0wIDAQAB");
 	var pattern_encrypted = encrypt.encrypt(pattern); 
-
 
 
 	// IMPRESIONES EN PANTALLA
@@ -305,262 +278,3 @@ function arrayBytesToBites(array, patron){
 
 	return stringInBites;
 }
-
-
-// // Función de codifica la cadena s dada a base64.
-// function base64_encode (s){
-//   // the result/encoded string, the padding string, and the pad count
-//   var base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-//   var r = ""; 
-//   var p = ""; 
-//   var c = s.length % 3;
-
-//   // add a right zero pad to make this string a multiple of 3 characters
-//   if (c > 0) { 
-//     for (; c < 3; c++) { 
-//       p += '='; 
-//       s += "\0"; 
-//     } 
-//   }
-
-//   // increment over the length of the string, three characters at a time
-//   for (c = 0; c < s.length; c += 3) {
-
-//     // we add newlines after every 76 output characters, according to the MIME specs
-//     /*if (c > 0 && (c / 3 * 4) % 76 == 0) { 
-//       r += "\r\n"; 
-//     }*/
-
-//     // these three 8-bit (ASCII) characters become one 24-bit number
-//     var n = (s.charCodeAt(c) << 16) + (s.charCodeAt(c+1) << 8) + s.charCodeAt(c+2);
-
-//     // this 24-bit number gets separated into four 6-bit numbers
-//     n = [(n >>> 18) & 63, (n >>> 12) & 63, (n >>> 6) & 63, n & 63];
-
-//     // those four 6-bit numbers are used as indices into the base64 character list
-//     r += base64chars[n[0]] + base64chars[n[1]] + base64chars[n[2]] + base64chars[n[3]];
-//   }
-//    // add the actual padding string, after removing the zero pad
-//   return r.substring(0, r.length - p.length) + p;
-// }
-
-// // Función que retorna una cadena de caracteres 0 y 1
-// // String = "text"
-// // Return = "0110100011001010111100001110100"
-// function stringToBinaryString(string){
-// 	let i = 0;
-// 	let binaryString = ""
-// 	for(i; i < string.length; i++)
-// 		binaryString += charToBinaryString(string[i]);
-// 	return binaryString;
-// }
-
-
-// // Funcion que retorna una cadena de 0 y 1
-// // Char = "t"
-// // Return = "01110100"
-// function charToBinaryString(char){
-// 	let num = char.charCodeAt(0);
-// 	return intToBinaryString(num);
-// }
-
-
-// //Function que retorna una cadena de 0 y 1
-// // int = 0x74
-// // Return = "01110100"
-// function intToBinaryString(int){
-// 	let mask = 0x80;
-// 	let string = "";
-// 	while(mask > 0){
-// 		if((int & mask) != 0)
-// 			string += '1';
-// 		else
-// 			string += '0';
-// 		mask = mask >> 1;
-// 	}
-
-// 	return string;
-// }
-
-
-
-
-// // Función que retorna la llave a utilizar para el AES
-// function getKey(){
-// 	return "Super llave de prueba";
-// }
-
-
-
-
-
-// // Función que crea el patrón aleatoriamente, cuya longitud es la longitud 
-// // del certificado más la longitud de la cabecera a inyectar por ocho puesto que es bite a bite 
-// function getPatternBite(LenCertificadoCharArray, LenCabeceraInyectar){
-
-// 	let lengthPc = (LenCabeceraInyectar+LenCertificadoCharArray)*8;
-	
-// 	// pcArray: contiene el patrón de chaffing 
-// 	// pcArray [byteControl, biteChaff, biteChaff, ..., biteChaff]
-// 	// 0 -> Bite Chaff : 1 -> Bite Original
-
-// 	let pcArray = new Array(lengthPc);
-// 	pcArray.fill(1,0,lengthPc);
-	
-// 	let n_0 = 0;
-// 	while(n_0 < LenCertificadoCharArray*8){
-
-// 		let random = getSecureRandomNumber() % lengthPc; // valores [0,lengthPc)
-// 		//random++; // Evitamos posición 0
-// 		if(pcArray[random] == 1){
-// 			pcArray[random] = 0;
-// 			n_0++;
-// 		}
-
-// 	}
-
-
-// 	// console.log("Número de ceros en pattern: "+n_0-1);
-// 	// console.log("PATRÓN CREADO EN BITES: "+pcArray.join("")+" "+pcArray.length);
-
-
-// 	return pcArray;
-// }
-
-
-// // Función que realiza el chaffing con base a el patrón (patternChaffing)
-// // si hay un 0 en el patrón, se coloca un carácter del certificado
-// // si hay un 1 en el patrón, se coloca un carácter de la cabecera 
-// function makeChaffingBite(patternChaffing, certArray, cabeceraInyectar, details){
-	
-// 	// stringChaffingCertificado : almacenará el chaffing entre el encabezado Accept y el Certificado
-// 	let stringChaffingCertificado = "";
-// 	let stringCabeceraInyectar = stringToBinaryString(cabeceraInyectar);
-// 	let stringCertArray = stringToBinaryString(certArray);
-
-// 	let contPcCharTot = 0; 		// 	contador de Certificado + Encabezado
-// 	let contCertificado = 0; 	// 	contador para certificado
-// 	let contEncabezado = 0; 	// 	contador para encabezado Mozilla
-	
-// 	while(contPcCharTot < patternChaffing.length){
-// 		if(patternChaffing[contPcCharTot] == 0)
-// 			stringChaffingCertificado += stringCertArray[contCertificado++]
-// 			//arrayChaffingCertificado[contPcCharTot] =  certArray[contCertificado++];
-// 		else
-// 			stringChaffingCertificado += stringCabeceraInyectar[contEncabezado++];
-// 		contPcCharTot++;
-// 	}
-
-
-// 	/*
-		
-// 		PARA CHAFFING
-
-// 	*/
-
-// 	console.log("CHAFFING CREADO EN BITES: "+stringChaffingCertificado+ "   "+stringChaffingCertificado.length);
-
-// 	let stringBytesChaffingCertificado = arrayBytesToBites(stringChaffingCertificado, false);
-// 	console.log("CHAFFING CON CARACTERES ESPECIALES: "+stringBytesChaffingCertificado+" "+stringBytesChaffingCertificado.length);
-
-// 	//stringBytesChaffingCertificado = "stringChaffingCertificado de prueba";
-
-// 	//PASAR A BASE64 EL CHAFFING
-// 	stringBytesChaffingCertificado = base64_encode(stringBytesChaffingCertificado);
-// 	console.log("CHAFFING EN BYTES (BASE64): " + stringBytesChaffingCertificado + " " + stringBytesChaffingCertificado.length);
-
-// 	//SE AGREGA UN NUEVO HEADER
-// 	details.requestHeaders.push({name:"Chaffing",value: stringBytesChaffingCertificado});
-
-
-// 	/*
-	
-// 		PARA PATTERN
-
-// 	*/
-
-
-// 	console.log("PATRON CREADO EN BITES: "+patternChaffing.join('')+ "  "+patternChaffing.length);
-// 	let patroninBytes = arrayBytesToBites(patternChaffing, false);
-
-// 	//patroninBytes = "stringChaffingCertificado de prueba";
-	
-// 	console.log("PATRON CREADO CON CARACTERES ESPECIALES: "+patroninBytes+"   "+patroninBytes.length);
-	
-// 	/*modo ctr*/
-// 	var key = getKey();
-// 	var options = { mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 };
-
-// 	patroninBytes_aes = CryptoJS.AES.encrypt(patroninBytes, key, options);
-// 	patroninBytes = patroninBytes_aes.toString()
-// 	console.log("PATRON CIFRADO CON AES: "+patroninBytes+"   "+patroninBytes.length);
-
-// 	var encrypt = new JSEncrypt();
-
-// 	encrypt.setPublicKey("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1fsvhHzUiUB20kciWsdCPf9gBiI6Z2cXnOH+VMkQtwXYhWyf0VXnV/cXGieXS5HvrZJvb0ldo8ZSqkaBy9BXrIAFswTgOxWfusa3nmL6YRzIHxI6FgpAt9xQAIKtnEWMShsufS/7FeR8Yam/u2qI2u+kM00ZPKQPOZPGQvEjy2QX88k/r88jP2a5UPzkSfg1vuAwMxGrVSuPcGrAUd2qJF6Slb1y6KvSo2KYLdnpv/us5MRKO+28u2QNr++uMIkyJz4Pqj67VUT2r1XThkdxAfPTgcRne15qQ2aDtlLqw8T6uo2xYNekZjuoUxenfxzikv3ejFgShT4bON6yzBtv0wIDAQAB");
-// 	var encrypted = encrypt.encrypt(key);
-
-// 	patroninBytes += " ";
-// 	patroninBytes += encrypted.toString();
-
-// 	/*
-// 		POR EL MOMENTO CODIFICADO EN BASE 64 PARA PODER MANDARLO EN RED
-// 		MÁS ADELANTE ESTO SE TENDRÍA QUE CAMBIAR A IMPLEMENTAR EL CIFRADO ASIMÉTRICO
-	
-// 	patroninBytes = base64_encode(patroninBytes);
-// 	console.log("PATRÓN CREADO (BASE64): "+patroninBytes+ "  "+patroninBytes.length);
-// 	*/
-
-
-// 	//SE AGREGA UN NUEVO HEADER DE PATTERN
-// 	details.requestHeaders.push({name:"Pattern",value: patroninBytes});
-
-// 	return details;
-// }
-
-
-
-
-// // Función que libera la nueva petición (newHeader)
-// function setFreeRequest(newHeader){
-// 	var pattern = "";
-//     var chaff = "";
-//     const url = newHeader.url;
-
-//     for(i = 0; i < newHeader.requestHeaders.length; i++){
-//         if(newHeader.requestHeaders[i].name.includes("Chaffing")){
-//             chaff = newHeader.requestHeaders[i].value;
-//             break;
-//         }
-//     }
-
-//     for(i = 0; i < newHeader.requestHeaders.length; i++){
-//         if(newHeader.requestHeaders[i].name.includes("Pattern")){
-//             pattern = newHeader.requestHeaders[i].value;
-//             break;
-//         }
-//     }
-
-//     console.log(newHeader);
-
-// 	//Liberación de la petición por medio de AJAX 
-// 	$.ajax({
-// 		url: url,
-// 		type: "GET",
-// 		contentType: "text/plain;charset=UTF-8",
-// 		datatype: 'text/plain',
-// 		headers: {
-// 			"Chaffing" : chaff,
-// 			"Pattern" : pattern
-// 		},
-// 		success:function(result){
-// 			console.log("ÉXITO AL ENVIAR PETICIÓN, IMPRIMIENDO RESPUESTA: ");
-//             console.log(result);
-//             window.open(result);
-// 		},
-// 		error:function(result){
-// 			console.log("ERROR AL ENVIAR PETICIÓN, IMPRIMIENDO RESPUESTA: ");
-// 			console.log(result);
-// 		}
-// 	})
-// }
