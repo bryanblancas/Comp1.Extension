b6a8401de700882a0f59607f566e953236ec562b
jerrycf <superjerry95@gmail.com>
2019-11-18 19:30:10 -0600

    Borrando comentarios lmao

--- background.js
+++ background.js
@@ -87,8 +87,6 @@ function chaffingProcess(certificadoCharArray, headers) {
 	let rtn = getPattern(550, 650);
 	let pattern = rtn[0];
 	let ones = rtn[1];
-	// console.log(pattern)
-	// console.log(ones)
 
 	let chaffing = makeChaffing(pattern, cert, ones);
 	// console.log(chaffing);
@@ -125,19 +123,9 @@ function getPattern(low, high){
 // Función que realiza el proceso de chaffing 
 function makeChaffing(pattern, certificadoCharArray, ones){
 
-	// pattern = [0,1,0,1]
-	// certificadoCharArray = ['a', 'b', '\n']
-	// ones = 2
-
 	let len_cert = certificadoCharArray.length
 	let len_pattern = pattern.length
 	let rep = Math.ceil(len_cert/ones)
-
-	// console.log("IMPRESIONES DE makeChaffing()")
-	// console.log("Longitud pattern: "+len_pattern)
-	// console.log("Longitud certificado: "+len_cert)
-	// console.log("Número de unos: "+ones)
-	// console.log("Repeticiones de patrón: "+len_cert+"/"+ones+"="+rep)
 
 	let chaffing = []
 
@@ -191,18 +179,12 @@ function fakeChar(){
 
 // Función que libera la nueva petición con el chaffing y pattern dados
 function freeRequest(headers, chaffing, pattern, len_cert){
-	// console.log("IMPRESIONES DE freeRequest()")
 
     const url = headers.url;
     chaffing = chaffing.join('');
     pattern = pattern.join('');
-	// console.log("PATTERN BITS: \n"+pattern)
 
-    // pattern = text 
-    // pattern = "01110100011001010111100001110100"
-    // pattern = "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
     pattern = arrayBytesToBites(pattern, 0);
-    // console.log("PATTERN BYTES LENGTH: "+pattern.length)
     chaffing = chaffing+" "+len_cert;
 
 
@@ -211,12 +193,6 @@ function freeRequest(headers, chaffing, pattern, len_cert){
 	encrypt.setPublicKey("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1fsvhHzUiUB20kciWsdCPf9gBiI6Z2cXnOH+VMkQtwXYhWyf0VXnV/cXGieXS5HvrZJvb0ldo8ZSqkaBy9BXrIAFswTgOxWfusa3nmL6YRzIHxI6FgpAt9xQAIKtnEWMShsufS/7FeR8Yam/u2qI2u+kM00ZPKQPOZPGQvEjy2QX88k/r88jP2a5UPzkSfg1vuAwMxGrVSuPcGrAUd2qJF6Slb1y6KvSo2KYLdnpv/us5MRKO+28u2QNr++uMIkyJz4Pqj67VUT2r1XThkdxAfPTgcRne15qQ2aDtlLqw8T6uo2xYNekZjuoUxenfxzikv3ejFgShT4bON6yzBtv0wIDAQAB");
 	var pattern_encrypted = encrypt.encrypt(pattern); 
 
-
-    // console.log("DIRECCIÓN: \n"+url)
-    // console.log("CHAFFING: \n"+chaffing)
-    // console.log("CHAFFING LENGTH: \n"+chaffing.length)
-    // console.log("PATTERN: \n"+pattern)
-    // console.log("PATTERN ENCRYPTED: \n"+pattern_encrypted)
 
 
 	//LIBERACIÓN DE LA PETICIÓN POR MEDIO DE AJAX 
@@ -243,34 +219,13 @@ function freeRequest(headers, chaffing, pattern, len_cert){
 	})
 }
 
-
-// Función que retorna un número aleatorio seguro (SecureRandom)
-// RandomSourse.getRandomValues()
-// Utiliza un arraglo que puede ser Int8Array, Uint8Array, 
-// Int16Array, Uint16Array, Int32Array o Uint32Array
-// Todos los elementos del array son escritos con números aleatorios seguros
-// Se utiliza un random normal para elegir la posición del número aleatorio a retornar
 function getSecureRandomNumber() {
     var array = new Uint16Array(10);
     window.crypto.getRandomValues(array);
     return array[Math.floor(Math.random() * 10)];
 }
 
-
-// Función que pasa una cadena de 0 y 1 que representan bites a bytes
-// "00101111" -> "/"
 function arrayBytesToBites(array, patron){
-	//PASAR EL ARREGLOS DE BYTES A BITES   -> 1 BYTE -> 1 BITE
-	/********************************************
-	*											*
-			EJEMPLO DE PATRÓN:
-		Array 		= 	"0010111101010010"
-		patronBytes_2 		= 	'00101111','10101000'
-		patronBytes_10 		= 	'0x2F = 47','0x52 = 82'
-		stringInBites 		= 		"R/"
-
-	*											*
-	********************************************/
 
 	let charCreado = 0;
 	let stringInBites = "";
